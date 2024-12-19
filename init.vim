4516a22eb778948b7c13a3b166161f34bfe1ff8e
Oscar Arvidsson <oscar.arvidsson@protonmail.com>
2024-12-19 17:18:40 +0100

    added generic install script

---
+++ init.vim
@@ -0,0 +1,22 @@
+filetype plugin indent on
+syntax on
+colo torte
+set mouse=a
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set autoindent
+set smartindent
+set number
+set showmatch
+set ignorecase
+set incsearch
+set nohlsearch
+set expandtab
+set nobackup
+set noswapfile
+set cursorline
+set cursorlineopt=number
+hi clear CursorLineNr
+hi CursorLineNr ctermfg=magenta
+set complete+=i
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if command -v apt 2>&1>/dev/null
+then
+sudo apt -y update
+sudo apt -y autoremove
+sudo apt -y upgrade
+sudo apt -y install neovim gcc make gdb git 
+fi
+
+git config --global init.defaultBranch master
+git config --global user.email "oscar.arvidsson@protonmail.com"
+git config --global user.name "Oscar Arvidsson"
+mkdir -p ~/.config/nvim
+cp init.vim ~/.config/nvim/init.vim
+cp gdbinit ~/.gdbinit
