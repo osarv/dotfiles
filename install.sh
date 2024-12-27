a2c0ed33e6957e6e32a6e229b3b57d60864ddac9
Oscar Arvidsson <oscar.arvidsson@protonmail.com>
2024-12-28 00:37:32 +0100

    added autoremote thingy

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ fi
 git config --global init.defaultBranch master
 git config --global user.email "oscar.arvidsson@protonmail.com"
 git config --global user.name "Oscar Arvidsson"
+git config --global push.autoSetupRemote true
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim/init.vim
 cp gdbinit ~/.gdbinit
