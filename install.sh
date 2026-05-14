0a4060ba2b59e009ef6a83c38b7518bf0f9df5f3
Oscar Arvidsson <oscar.arvidsson@protonmail.com>
2026-05-14 10:20:13 +0200

    added termux installation

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-if command -v apt 2>&1>/dev/null
-then
-sudo apt -y update
-sudo apt -y autoremove
-sudo apt -y upgrade
-sudo apt -y install neovim gcc make gdb git 
+if command -v apt 2>&1>/dev/null; then
+    if command -v sudo 2>&1>/dev/null; then
+        #full system path
+        sudo apt -y update
+        sudo apt -y autoremove
+        sudo apt -y upgrade
+        sudo apt -y install neovim gcc make gdb git 
+    else
+        #termux path
+        apt -y update
+        apt -y autoremove
+        apt -y upgrade
+        apt -y install neovim clang make gdb git
+    fi
 fi
+
 
 git config --global init.defaultBranch master
 git config --global user.email "oscar.arvidsson@protonmail.com"
